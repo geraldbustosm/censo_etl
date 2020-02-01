9c46b85a031097daee8eb04338181e0fa22cb1be
Gerald Bustos Morales <geraldbustosm@gmail.com>
2020-02-01 05:00:23 -0300

    test listas methods

--- censo.py
+++ censo.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import listas as ls
 
-print(ls.lista_paises())
-print(ls.lista_comunas())
-print(ls.lista_pueblos())
+#print(ls.lista_paises())
+#print(ls.lista_comunas())
+#print(ls.lista_pueblos())
+#print(ls.lista_provincias())
+#print(ls.lista_regiones())
 # Leyendo csv
 '''tf = pd.read_csv('Microdato_Censo2017-Personas.csv', sep=';', chunksize=500000)
 
